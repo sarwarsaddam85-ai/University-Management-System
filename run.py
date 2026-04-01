d120e7e66af7681f607bab95bfb5481246e68544
Muhammad Usman <us25465@gmail.com>
2026-04-01 13:06:00 +0500

    Update host and port for Render

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # This line is the magic: it checks if Render gave us a Port. 
+    # If not (on your PC), it uses 5000.
+    port = int(os.environ.get("PORT", 5000))
+    
+    # host='0.0.0.0' allows Render to send people to your site
+    app.run(host='0.0.0.0', port=port)
